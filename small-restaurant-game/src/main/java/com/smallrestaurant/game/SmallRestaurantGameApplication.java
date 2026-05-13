717423cd28cc73240ad83cfa10e47eb09eefe14d
王宇瑄 <wangyuxuanEker@outlook.com>
2026-05-13 14:59:23 +0800

    第一次提交

---
+++ small-restaurant-game/src/main/java/com/smallrestaurant/game/SmallRestaurantGameApplication.java
@@ -0,0 +1,9 @@
+package com.smallrestaurant.game;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SmallRestaurantGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmallRestaurantGameApplication.class, args);
+    }
+}
